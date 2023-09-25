95ad4440dad6378f1bc16ce5aef4de2610c31c27
Samanvith <Samanvith2005@gmail.com>
2023-09-25 11:28:04 +0530

    Api Call(Fetch)

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Use Routes i
 import Header from './Components/Header';
 import About from './Components/About';
 import Contact from './Components/Contact';
-import NotFound from './Components/Notfound';
+import NotFound from './Components/NotFound';
 import BlogList from './Components/BlogList';
 import BlogItemDetails from './Components/BlogItemDetails';
 import './App.css';
--- blog-app/src/Components/BlogItemDetails/index.js
+++ blog-app/src/Components/BlogItemDetails/index.js
@@ -1,37 +1,43 @@
-import { Component } from 'react'
-import Loader from 'react-loader-spinner'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Loader from 'react-loader-spinner';
 
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
-import './index.css'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import './index.css';
 
-class BlogItemDetails extends Component {
-  state = { blogData: {}, isLoading: true }
+const BlogItemDetails = () => {
+  const { id } = useParams();
+  const [blogData, setBlogData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-  componentDidMount() {
-    this.getBlogItemData()
-  }
+  useEffect(() => {
+    const fetchBlogItemData = async () => {
+      try {
+        const response = await fetch(`https://apis.ccbp.in/blogs/${id}`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        const updatedData = {
+          title: data.title,
+          imageUrl: data.image_url,
+          content: data.content,
+          avatarUrl: data.avatar_url,
+          author: data.author,
+        };
+        setBlogData(updatedData);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching blog data:', error);
+        setIsLoading(false);
+      }
+    };
 
-  getBlogItemData = async () => {
-    const { match } = this.props
-    const { params } = match
-    const { id } = params
+    fetchBlogItemData();
+  }, [id]);
 
-    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
-    const data = await response.json()
-
-    const updatedData = {
-      title: data.title,
-      imageUrl: data.image_url,
-      content: data.content,
-      avatarUrl: data.avatar_url,
-      author: data.author,
-    }
-    this.setState({ blogData: updatedData, isLoading: false })
-  }
-
-  renderBlogItemDetails = () => {
-    const { blogData } = this.state
-    const { title, imageUrl, content, avatarUrl, author } = blogData
+  const renderBlogItemDetails = () => {
+    const { title, imageUrl, content, avatarUrl, author } = blogData;
 
     return (
       <div className="blog-info">
@@ -43,22 +49,18 @@ class BlogItemDetails extends Component {
         <img className="blog-image" src={imageUrl} alt={title} />
         <p className="blog-content">{content}</p>
       </div>
-    )
-  }
+    );
+  };
 
-  render() {
-    const { isLoading } = this.state
+  return (
+    <div className="blog-container">
+      {isLoading ? (
+        <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+      ) : (
+        renderBlogItemDetails()
+      )}
+    </div>
+  );
+};
 
-    return (
-      <div className="blog-container">
-        {isLoading ? (
-          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
-        ) : (
-          this.renderBlogItemDetails()
-        )}
-      </div>
-    )
-  }
-}
-
-export default BlogItemDetails
+export default BlogItemDetails;
